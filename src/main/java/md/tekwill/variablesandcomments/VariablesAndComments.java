02286d9bdaa59aabb36efba61df9ac3b8b232f23
Zinaida Bizgan <zinaida.bizgan@smartsoftdev.eu>
2024-10-14 22:30:31 +0300

    Added the variables and Comments task

---
+++ src/main/java/md/tekwill/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,42 @@
+package md.tekwill.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+        // Declare the primitive data type variables
+
+        boolean falseBooleanVariables;
+        boolean trueBooleanVariables;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+        // Initialisation of the above variables
+
+        falseBooleanVariables = false;
+        trueBooleanVariables = true;
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -213648;
+        upperLimitOfIntDataType = 214747;
+        lowerLimitOfLongDataType = -922354775;
+        upperLimitOfLongDataType = 922354775;
+        lowerLimitOfFloatDataType = -64.5f;
+        upperLimitOfFloatDataType = 64.5f;
+        lowerLimitOfDoubleDataType = 5.555;
+        upperLimitOfDoubleDataType = 556.555;
+    }
+}
